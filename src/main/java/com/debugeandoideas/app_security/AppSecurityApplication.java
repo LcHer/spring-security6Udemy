44a9d475c6467f75fcde49b4552db8bbd7da0685
Cecilia Hdz <ceciferch@gmail.com>
2026-06-08 17:00:45 -0600

    se suben cambios de la seccion 3 del curso, en el cual se vio el passwordEncoder

--- src/main/java/com/debugeandoideas/app_security/AppSecurityApplication.java
+++ src/main/java/com/debugeandoideas/app_security/AppSecurityApplication.java
@@ -1,15 +1,20 @@
 package com.debugeandoideas.app_security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableWebSecurity //esto ya es opcional en la version 6 de spring security
-public class AppSecurityApplication {
+public class AppSecurityApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppSecurityApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/debugeandoideas/app_security/security/SecurityConfig.java
+++ src/main/java/com/debugeandoideas/app_security/security/SecurityConfig.java
@@ -4,14 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -46,10 +40,9 @@ public class SecurityConfig {
     UserDetailsService userDetailsService (DataSource dataSource){
         return new JdbcUserDetailsManager(dataSource);
     }*/
-
     @Bean
-    PasswordEncoder bCryptPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
     }
 
 }
